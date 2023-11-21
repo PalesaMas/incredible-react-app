159c925f080cfd446f2dd85fc81b678721de58f0
Palesa <palesa.masiteng@gmail.com>
2023-11-21 16:30:52 +0200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,24 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This projected was coded by{" "}
+          href="https://precious-zabaione-7d1d9f.netlify.app/about"
+          target="_blank"
+        >
+          Palesa Masiteng{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/PalesaMas/incredible-react-app"
+          target="_blank"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
