960d211a7a5035ee062e373b584d7d50fc3b89dd
Palesa <palesa.masiteng@gmail.com>
2023-11-24 23:42:06 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This projected was coded by{" "}
-        <a
-          href="https://precious-zabaione-7d1d9f.netlify.app/about"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Palesa Koena Masiteng{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/PalesaMas/incredible-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This projected was coded by{" "}
+          <a
+            href="https://precious-zabaione-7d1d9f.netlify.app/about"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Palesa Koena Masiteng{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/PalesaMas/incredible-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
