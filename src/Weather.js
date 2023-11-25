f3421a61532d4036d1ee86cb9adb68bf73ca6f2d
Palesa <palesa.masiteng@gmail.com>
2023-11-25 13:46:31 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -8,12 +8,17 @@ export default function Weather() {
           <div className="col-9">
             <input
               type="search"
-              placeholder="Enter a City..."
+              placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -22,13 +27,19 @@ export default function Weather() {
         <li>Friday 23:56</li>
         <li>Mostly Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Sunny"
-          />
-          28°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Sunny"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">28</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
