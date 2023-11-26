ff03bb86c5beb1792da90755026fab88b4fe7e8e
Palesa <palesa.masiteng@gmail.com>
2023-11-26 16:58:04 +0200

    Created new component and search engine event listener

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Berlin" />
+        <Weather defaultCity="Johannesburg" />
         <footer>
           This project was coded by{" "}
           <a
